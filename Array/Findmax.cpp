c6e05634366d5883d96a2d15c9aca3a2d385b656
shivam rajput <111221987+svm7456@users.noreply.github.com>
2023-06-27 10:31:39 +0530

    Update Findmax.cpp

--- Array/Findmax.cpp
+++ Array/Findmax.cpp
@@ -1,1 +1,11 @@
 
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    int max=INT_MIN;
+    for(int i=0;i<n;i++){
+       if(arr[i]>max){
+           max=arr[i];
+       }
+    }
+    return max;
+}
