400683a0aa8e56afd80ce1132fa52151d79daae0
shivam rajput <111221987+svm7456@users.noreply.github.com>
2023-07-04 08:19:59 +0530

    Update FindTheNumberAppearOnce.cpp

--- Array/FindTheNumberAppearOnce.cpp
+++ Array/FindTheNumberAppearOnce.cpp
@@ -1,1 +1,32 @@
+ unordered_map<int,int> m;
+    int n;
+    cout<<"Please enter the size of the array \n";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        int ele=arr[i];
+        auto it=m.find(ele);
+        if(it==m.end()){
+            m.insert({ele,1});
+        }
+        else
+        {
+            it->second=it->second+1;
+        }
+    }
+    for(auto it : m){
+       auto x=it.find(1);
+       if(x==it.end()){
+           cout<<"Not found ";
+        }
+        else
+        {
+            cout<<"Answer is "<< x.first<<" ";
+        }
+    }
+    
 
+    return 0;
